ae12b79e3739e3fb5596b9dbb2bfa044ef497e03
Samuel Mirakov <118082054+SamuelMirakov@users.noreply.github.com>
2023-06-12 15:59:38 -0400

    Create btNode.java

---
+++ btNode.java
@@ -0,0 +1,20 @@
+// NODE CLASS
+public class btNode {
+  public	btNode left, right;
+	Customer data = new Customer();
+/*
+	 // Basic Constructor with Customer parameter
+	 public btNode(Customer curr) {
+	 	data = curr;
+	 	right = left = null;
+	 }
+
+	// // Basic Constructors
+	 public btNode() {
+	 	this.data = null;
+	 	this.left = null;
+	 	this.right = null;
+	 }
+   */
+
+}
